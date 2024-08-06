a52fffa9a2a40caf5f71a892de82561339644b3a
Pranjal Shinde <shindept2003@gmail.com>
2024-08-07 00:20:58 +0530

    Connected to atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const mongoURL = 'mongodb://localhost:27017/codetogether';
+const mongoURL =  process.env.MONGOURL;
 
 mongoose.connect(mongoURL, {})
 
